7fb999347e3693fd567f9b82a081bde50ee78c27
R Venkatesh <venkateshvenky4910@gmail.com>
2026-07-18 08:31:56 +0530

    Time: 1 ms (99.98%) | Memory: 42.9 MB (11.51%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Solution {
+    public static int reverse(int x) {
+        int r=0;
+     while(x!=0){
+        int t= x % 10;
+        if (r > Integer.MAX_VALUE / 10 || (r == Integer.MAX_VALUE / 10 && t > 7)) {
+                return 0;
+            }
+        if (r < Integer.MIN_VALUE / 10 || (r == Integer.MIN_VALUE / 10 && t < -8)) {
+                return 0;
+            }
+        r=r*10 + t;
+        x=x / 10;
+     } 
+     return r;
+    }
+    public static void main (String []args) {
+        Scanner sc = new Scanner (System.in);
+        int num=sc.nextInt();
+        System.out.println(reverse(num));
+        sc.close();
+    }
+}
